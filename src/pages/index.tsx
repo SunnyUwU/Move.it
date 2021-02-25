8926ee1637cc40d83a9cf8f12a074be3a7760d5d
Gybson Klarck <68571007+SunnyUwU@users.noreply.github.com>
2021-02-25 15:48:16 -0300

    Update Contexts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head';
 
 import styles from '../style/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -16,17 +17,19 @@ export default function Home() {
       </Head>
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
